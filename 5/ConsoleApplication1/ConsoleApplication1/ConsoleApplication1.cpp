743340f499c7681e6ae9839f88db3c24467c6104
Maxim <70898828+SaintOwl@users.noreply.github.com>
2021-01-18 13:33:19 +0300

    Add files via upload

---
+++ 5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,92 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <icmpapi.h>
+#include <iostream>
+
+using namespace std;
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+void MyPing(const char* IP, unsigned int Timeout, unsigned int RequestCount) {
+    HANDLE hIP = IcmpCreateFile();
+    if (hIP == INVALID_HANDLE_VALUE) {
+        WSACleanup();
+        return;
+    }
+
+    char SendData[32] = "Data for ping";
+    int LostPacketsCount = 0;
+    unsigned int MaxMS = 0;
+    int MinMS = -1;
+
+    PICMP_ECHO_REPLY pIpe = (PICMP_ECHO_REPLY)GlobalAlloc(GHND, sizeof(ICMP_ECHO_REPLY) + sizeof(SendData));
+    if (pIpe == 0) {
+        IcmpCloseHandle(hIP);
+        WSACleanup();
+        return;
+    }
+    pIpe->Data = SendData;
+    pIpe->DataSize = sizeof(SendData);
+    unsigned long ipaddr = inet_addr(IP);
+
+    for (unsigned int i = 0; i < RequestCount; i++) {
+        int dwStatus = IcmpSendEcho(hIP, ipaddr, SendData, sizeof(SendData), NULL, pIpe, sizeof(ICMP_ECHO_REPLY) + sizeof(SendData), Timeout);
+
+        if (dwStatus > 0) {
+            for (int i = 0; i < dwStatus; i++) {
+                unsigned char* pIpPtr = (unsigned char*)&pIpe->Address;
+                cout << "Ответ от " << (int)*(pIpPtr) << "." << (int)*(pIpPtr + 1)
+                    << "." << (int)*(pIpPtr + 2) << "." << (int)*(pIpPtr + 3)
+                    << ": число байт = " << pIpe->DataSize << " время = " << pIpe->RoundTripTime
+                    << "мс TTL = " << (int)pIpe->Options.Ttl;
+
+                MaxMS = (MaxMS > pIpe->RoundTripTime) ? MaxMS : pIpe->RoundTripTime;
+                MinMS = (MinMS < (int)pIpe->RoundTripTime&& MinMS >= 0) ? MinMS : pIpe->RoundTripTime;
+                cout << endl;
+            }
+        }
+        else {
+            if (pIpe->Status) {
+                LostPacketsCount++;
+                switch (pIpe->Status) {
+                case IP_DEST_PORT_UNREACHABLE:
+                    cout << "Remote host may be down." << endl;
+                    break;
+                case IP_REQ_TIMED_OUT:
+                    cout << "Request timed out." << endl;
+                    break;
+                case IP_TTL_EXPIRED_TRANSIT:
+                    cout << "TTL expired in transit." << endl;
+                    break;
+                default:
+                    cout << "Error code: %ld" << pIpe->Status << endl;
+                    break;
+                }
+            }
+        }
+    }
+
+    IcmpCloseHandle(hIP);
+    WSACleanup();
+
+    if (MinMS < 0) MinMS = 0;
+    cout << endl << "Статистика Ping " << IP;
+    cout << "\tПакетов: отправлено = " << RequestCount << ", получено = " << RequestCount - LostPacketsCount
+        << ", потеряно = " << LostPacketsCount << "<" << (int)(100 / (float)RequestCount) * LostPacketsCount << " % потерь>, " << endl;
+    cout << "Приблизительное время приема-передачи:" << endl << "Минимальное = " << MinMS << "мс, Максимальное = " << MaxMS
+        << "мс, Среднее = " << (MaxMS + MinMS) / 2 << "мс" << endl;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "RUS");
+    /*Ping("216.58.206.14", 1000, 10);*/
+    MyPing("127.0.0.1", 1000, 10);
+
+    system("pause");
+    return 0;
+}
---
+++ 6/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+
+int* ipCheck(string IP) {
+
+	cout << "----------IP----------" << endl;
+	string buff;
+	int* ipOct = new int[4];
+	int j = 0;
+	for (int i = 0, dot = 0; dot < 4 ; i++) {
+		if (IP[i] != '.') {
+			buff += IP[i];
+		}
+		else {
+			ipOct[j] = stoi(buff);
+			j++;
+			buff = "";
+			dot++;
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		cout << "Октет " << i + 1 << ", значение " << ipOct[i] << endl;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (ipOct[i] > 255 || ipOct[i] < 0) {
+			cout << "Ошибка! Октет " << i + 1 << " не входит в диапазон(0;255)! Значение октета " << ipOct[i] << "!" << endl;
+			exit(0);
+		}
+	}
+
+	return ipOct;
+}
+
+
+int* maskCheck(string MASK) {
+
+	cout << "----------МАСКА----------" << endl;
+	string buff;
+	int* maskOct = new int[4];
+	int j = 0;
+	for (int i = 0, dot = 0; dot < 4; i++) {
+		if (MASK[i] != '.') {
+			buff += MASK[i];
+		}
+		else {
+			maskOct[j] = stoi(buff);
+			j++;
+			buff = "";
+			dot++;
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		cout << "Октет " << i + 1 << ", значение " << maskOct[i] << endl;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (maskOct[i] > 255 || maskOct[i] < 0) {
+			cout << "Ошибка! Октет " << i + 1 << " не входит в диапазон(0;255)! Значение октета " << maskOct[i] << "!" << endl;
+			exit(0);
+		}
+	}
+
+	//ПОСЛЕДОВАТЕЛЬНОСТЬ БИТ
+	int a, i, bitBuff[99];
+	int numOf = 0;
+	for (int bitP = 0; bitP < 4; bitP++) {
+		string BinNum = "";
+		//cout << "Введите число: " << endl;
+		//cin >> a;
+		a = maskOct[bitP];
+		i = 0;
+		while (a != 0)
+		{
+			if (a % 2 == 0)
+			{
+				bitBuff[i] = 0;
+				a = a / 2;
+			}
+			else
+			{
+				bitBuff[i] = 1;
+				a = a / 2;
+			}
+			i++;
+		}
+		i = i - 1;
+		for (int j = 0; i >= 0; i--, j++) {
+			BinNum += to_string(bitBuff[i]);
+		}
+		if (BinNum == "") {
+			cout << "00000000" << '.';
+		}
+		else
+		cout << BinNum << '.';
+
+		if (BinNum == "" || BinNum == "10000000" || BinNum == "11000000" || BinNum == "11100000" || BinNum == "11110000" || BinNum == "11111000" || BinNum == "11111100" || BinNum == "11111110" || BinNum == "11111111") {
+			numOf += 1;
+		}
+	}
+	if (numOf == 4) {
+		cout << "\nПоследовательность соблюдается!" << endl;
+	}
+	else
+		cout << "\nПоследовательность не соблюдается!" << endl;
+	return maskOct;
+}
+
+
+int* NetworkID(int* IP, int* MASK) {
+	int* subnet = new int[4];
+	cout << "Network ID: \n\t";
+	for (int i = 0; i < 4; i++) {
+		subnet[i] = IP[i] & MASK[i];
+		cout << subnet[i] << ".";
+	}
+	return subnet;
+}
+
+
+void HostID(int* IP, int* MASK) {
+	int* host = new int[4];
+	cout << "Host ID: \n\t";
+	for (int i = 0; i < 4; i++) {
+		host[i] = IP[i] & ~MASK[i];
+		cout << host[i] << ".";
+	}
+}
+
+void Broadcast(int* subnet, int* MASK) {
+	int* broadcast = new int[4];
+	cout << "Broadcast: \n\t";
+	for (int i = 0; i < 4; i++) {
+
+		broadcast[i] = subnet[i] | ~MASK[i];
+		cout << broadcast[i] << ".";
+	}
+}
+
+/*void toBin() {
+	int a, i, buff[99];
+	string BinNum = "";
+	cout << "Введите число: " << endl;
+	cin >> a;
+	i = 0;
+	while (a != 0)
+	{
+		if (a % 2 == 0)
+		{
+			buff[i] = 0;
+			a = a / 2;
+		}
+		else
+		{
+			buff[i] = 1;
+			a = a / 2;
+		}
+		i++;
+	}
+	i = i - 1;
+	for (int j = 0; i >= 0; i--, j++) {
+		BinNum += to_string(buff[i]);
+		cout << buff[i];
+	}
+	cout << BinNum;
+
+	if (BinNum == "" || BinNum == "10000000" || BinNum == "11000000" || BinNum == "11100000" || BinNum == "11110000" || BinNum == "11111000" || BinNum == "11111100" || BinNum == "11111110" || BinNum == "11111111") {
+		cout << "Последовательность соблюдается!" << endl;
+	}
+	else {
+		cout << "Последовательность не соблюдается!" << endl;
+	}
+}*/
+
+void main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	string ip;
+	string mask;
+
+	cout << "Введите IP: " << endl;
+	ip = "192.168.1.100.";
+	cout << ip <<endl;
+
+	cout << "Введите маску: " << endl;
+	mask = "255.255.255.0.";
+	cout << mask << endl;
+
+	int* ipArr = new int[4];
+	ipArr = ipCheck(ip);
+
+	int* maskArr = new int[4];
+	maskArr = maskCheck(mask);
+
+	/*for (int i = 0; i < 4; i++) {
+		cout << ipArr[i] << endl;
+		cout << maskArr[i] << endl;
+	}*/
+
+	int* subnetArr = new int[4];
+	subnetArr = NetworkID(ipArr, maskArr);
+	cout << endl;
+	HostID(ipArr, maskArr);
+	cout << endl;
+	//Broadcast(subnetArr, maskArr);
+	//cout << endl;
+	system("pause");
+
+	//toBin();
+}
